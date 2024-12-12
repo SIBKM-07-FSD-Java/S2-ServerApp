28637d2453e872ade354808f2cdfe6da33dc91bc
fuzi <fzein.1695@gmail.com>
2024-12-12 09:42:25 +0700

    fix: endpoint

--- src/main/java/com/sibkm/serverapp/controller/AuthController.java
+++ src/main/java/com/sibkm/serverapp/controller/AuthController.java
@@ -13,12 +13,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping
 public class AuthController {
 
   private AuthService authService;
